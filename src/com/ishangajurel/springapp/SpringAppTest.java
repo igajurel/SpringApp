0d7415b9cb3e63f9a6eab2f794defe6738d49fb9
Ishan Gajurel <ishangazurel@gmail.com>
2018-08-21 17:58:18 -0500

    writing first unit test

--- src/com/ishangajurel/springapp/SpringAppTest.java
+++ src/com/ishangajurel/springapp/SpringAppTest.java
@@ -5,6 +5,10 @@ import junit.framework.TestCase;
 public class SpringAppTest extends TestCase {
 
     public void testGetAge() {
+        int age = 109;
+        SpringApp sa = new SpringApp();
+        sa.setAge(age);
+        assertEquals(sa.getAge(),age);
     }
 
     public void testSetAge() {
