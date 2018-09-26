49f02d4e94488129f1a3bd0df28184ce781d359a
igajurel <ishangazurel@gmail.com>
2018-09-25 22:06:14 -0500

    added buttons and actionevent listeners

---
+++ src/todo/ui/button/ActionListenerButton.java
@@ -0,0 +1,17 @@
+package todo.ui.button;
+
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+
+public class ActionListenerButton extends JButton {
+
+    private ActionListener actionListener;
+
+    public void setActionListener(ActionListener actionListener) {
+        this.actionListener = actionListener;
+    }
+
+    public void init(){
+        this.addActionListener(actionListener);
+    }
+}
---
+++ src/todo/ui/button/ListTableActionListener.java
@@ -0,0 +1,22 @@
+package todo.ui.button;
+
+import javax.swing.JTable;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+public abstract class ListTableActionListener implements ActionListener {
+
+    protected JTable table;
+    protected List list;
+
+    public void setList(List list){
+        this.list = list;
+    }
+
+    public void setTable(JTable itemTable){
+        this.table = itemTable;
+    }
+
+
+
+}
